c6e3d19a3a67dd0a3720dc52f88d8cc9ee334897
nadimdaud <nadim.daud@reltio.com>
2025-12-16 00:32:44 +0000

    Dependencies of JSON parsing added, Logic for retrieval of past runs and print of them added. Support for stoppage implemented

--- src/Monitor.java
+++ src/Monitor.java
@@ -1,27 +1,73 @@
-public class Monitor implements Runnable {
+import java.io.FileWriter;
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.time.Instant;
 
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.jetbrains.annotations.Nullable;
+
+public class Monitor implements Runnable {
+    private static final ObjectMapper mapper = new ObjectMapper();
+    private String owner;
     private String repo ;
-    private String lastseen ;
     private String token ;
+    private Instant timestamp;
     private boolean running = true;
-    public Monitor (String repo, String timestamp, String token ) {
+    private final HttpClient httpClient;
+
+
+    private final String GET_WORKFLOWS_RUNS = String.format("https://api.github.com/repos/%s/%s/actions/runs", owner, repo);
+    private final String GET_RUNS_JOBS = String.format("https://api.github.com/repos/%s/%s/actions/runs", owner, repo);
+    private final String GET_JOBS_STEPS = String.format("https://api.github.com/repos/%s/%s/actions/runs", owner, repo);
+
+    public Monitor (String repo, @Nullable Instant timestamp, String token , String owner) {
+        this.timestamp = timestamp;
         this.repo = repo;
-        this.lastseen = timestamp;
         this.token = token;
+        this.owner = owner;
+        httpClient = HttpClient.newBuilder()
+                    .version(HttpClient.Version.HTTP_2)
+                    .build();
     }
 
     @Override
     public void run() {
-        running = false;
-    }
+        HttpRequest.Builder requestBase =  HttpRequest.newBuilder()
+                .header("Authorization", "Bearer " + token)
+                .header("Accept", "application/vnd.github+json")
+                .GET();
+        HttpRequest requestRuns = requestBase
+                .uri(URI.create(GET_WORKFLOWS_RUNS))
+                .build();
 
-    public void stop() {
         while (running) {
             try {
-                // 1. Call GitHub API (list runs/jobs/steps since lastSeen)
-                // 2. Diff with previous state, emit events to sink
-                // 3. Update lastSeen
-                // 4. Sleep a bit
+                HttpResponse<String> responseRuns = httpClient.send(
+                        requestRuns,
+                        HttpResponse.BodyHandlers.ofString()
+                );
+                JsonNode workflowRuns = mapper.readTree(responseRuns.body());
+                int runsNumber = workflowRuns.get("total_count").asInt();
+                Instant workflowTime = timestamp;
+                for(int i = 0; i < runsNumber; i++) {
+                    JsonNode array = workflowRuns.get("workflow_runs");
+                    for (JsonNode node : array) {
+                        Instant time = Instant.parse(node.get("updated_at").asText());
+
+                        if (timestamp == null || time.isAfter(workflowTime)) {
+                            System.out.println("This is some info for a workflow run");
+                        }
+                        // Find the most recent time
+                        if (workflowTime == null || time.isAfter(workflowTime)) {
+                            workflowTime = time;
+                        }
+                    }
+                }
+                timestamp = workflowTime;
                 Thread.sleep(5000);
             } catch (InterruptedException e) {
                 // if we ever use interrupt, we can also break here
@@ -30,6 +76,14 @@ public class Monitor implements Runnable {
             } catch (Exception e) {
                 e.printStackTrace();
             }
+        }
+    }
+
+    public void stop() throws IOException {
+        running = false;
+        //write into .txt timestamp together with repo url
+        try (FileWriter writer = new FileWriter("Repositories.txt", true)) {
+            writer.write(String.format("%s %s\n", repo, timestamp.toString()));
         }
     }
 }
--- src/main.java
+++ src/main.java
@@ -1,5 +1,6 @@
 import java.io.*;
 import java.nio.file.Files;
+import java.time.Instant;
 import java.util.List;
 
 public class main {
@@ -8,22 +9,26 @@ public class main {
     static final File file = new File(FILENAME);
 
     public static void main(String[] args) throws IOException, InterruptedException {
-        String repoUrl = args[0];
+        String[] urlParts = args[0].split("/");
+        String repo = urlParts[4];
+        String owner = urlParts[3];
         String token = args[1];
         String timestamp = null;
+
+        if(!file.exists()) file.createNewFile();
 
         List<String> lines = Files.readAllLines(file.toPath());
         for (String line : lines) {
             String[] info = line.split(" ");
-            if(info[0].equals(repoUrl)) timestamp = info[1];
+            if(info[0].equals(repo)) timestamp = info[1];
         }
 
-        Monitor monitor = new Monitor(repoUrl, timestamp, token);
+        Monitor monitor = new Monitor(repo, timestamp==null ? null : Instant.parse(timestamp) , token, owner);
 
         Thread monitorThread = new Thread(monitor, "monitor-thread");
         monitorThread.start();
 
-        System.out.println("Monitoring " + repoUrl + "...");
+        System.out.println("Monitoring " + repo + "...");
         System.out.println("Press ENTER to stop.");
 
         try (BufferedReader br = new BufferedReader(new InputStreamReader(System.in))) {
